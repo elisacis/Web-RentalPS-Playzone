e7cb97098059579fbe03538976acf208ebaa9502
alQhafari <qhafari123@gmail.com>
2024-05-20 01:36:49 +0700

    Create About Us Page without footer and CTU

---
+++ src/js/about-us.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Function for Slider
+  const slider = document.querySelector(".slider-wrapper");
+  const slides = document.querySelectorAll(".slide");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  let currentSlide = 0;
+
+  function updateSliderPosition() {
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+  }
+
+  prevButton.addEventListener("click", function () {
+    currentSlide = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
+    updateSliderPosition();
+  });
+
+  nextButton.addEventListener("click", function () {
+    currentSlide = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
+    updateSliderPosition();
+  });
+
+  window.addEventListener("resize", updateSliderPosition);
+
+  // Function to load navbar HTML
+  function loadNavbar() {
+    fetch("src/components/navbar/navbar.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById("navbar").innerHTML = data;
+        addActiveClass(); // Add active class to current page link
+        setupToggleButton(); // Setup toggle button click event
+        setupScrollBehavior(); // Setup scroll behavior
+      })
+      .catch((error) => console.log("Error loading navbar:", error));
+  }
+
+  // Function to add active class to current page link
+  function addActiveClass() {
+    var currentLocation = window.location.href;
+    var navLinks = document.querySelectorAll("#navbar a");
+
+    navLinks.forEach(function (link) {
+      if (link.href === currentLocation) {
+        link.classList.add("active");
+      }
+    });
+  }
+
+  function setupToggleButton() {
+    const mobileMenu = document.querySelector("header nav .mobile-menu");
+    const menu = document.querySelector("header nav .menu");
+    mobileMenu.addEventListener("click", function () {
+      menu.classList.toggle("show");
+    });
+    // Remove class kalau diklik diluar
+    document.addEventListener("click", function (event) {
+      if (!menu.contains(event.target) && !mobileMenu.contains(event.target)) {
+        menu.classList.remove("show");
+      }
+    });
+  }
+
+  function setupScrollBehavior() {
+    const header = document.querySelector("header");
+    window.onscroll = function () {
+      if (document.documentElement.scrollTop >= 200) {
+        console.log("test");
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+  }
+
+  // Load the navbar when the DOM is ready
+  loadNavbar();
+});
