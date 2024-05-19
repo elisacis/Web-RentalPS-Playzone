a361f2251cdb730788400502297a3336f342d4fe
Ozik Jarwo <jarwoozik08@gmail.com>
2024-05-19 19:51:28 +0700

    Index.html : all section

--- src/js/index.js
+++ src/js/index.js
@@ -52,3 +52,48 @@ document.addEventListener("DOMContentLoaded", function () {
   // Load the navbar when the DOM is ready
   loadNavbar();
 });
+
+// Hero button smooth scroll to #recent
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelector("#hero .text .button").addEventListener("click", function() {
+      var targetElement = document.querySelector("#recent");
+      var targetOffset = targetElement.offsetTop - 60;
+      window.scrollTo({
+        top: targetOffset,
+        behavior: "smooth"
+      });
+  });
+});
+
+// Testimonial Slider
+
+var testimonials = document.querySelectorAll('.testimonial');
+var navigationContainer = document.querySelector('.navigation');
+var currentIndex = 0;
+function showTestimonial(index) {
+  var navigationButtons = navigationContainer.querySelectorAll('button');
+  navigationButtons.forEach(function(button) {
+    button.classList.remove('active');
+  });
+  navigationButtons[index].classList.add('active');
+  var testimonialContainer = document.querySelector('.slider-container');
+  testimonialContainer.style.transform = 'translateX(-' + (index * 100) + '%)';
+}
+function nextTestimonial() {
+  currentIndex++;
+  if (currentIndex >= testimonials.length) {
+    currentIndex = 0;
+  }
+  showTestimonial(currentIndex);
+}
+var autoplayInterval = setInterval(nextTestimonial, 3000);
+testimonials.forEach(function(testimonial, index) {
+  var button = document.createElement('button');
+  button.addEventListener('click', function() {
+    clearInterval(autoplayInterval);
+    currentIndex = index;
+    showTestimonial(index);
+  });
+  navigationContainer.appendChild(button);
+});
+showTestimonial(currentIndex);
