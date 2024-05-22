065336e1e50680b9c581610f2df6da328b29c32f
Ozik Jarwo <jarwoozik08@gmail.com>
2024-05-22 21:18:09 +0700

    Navbar big update for performance & SEO

---
+++ src/js/all.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+document.querySelector('.mobile-nav').addEventListener('click', () => {
+    document.querySelector('header nav').classList.toggle('show');
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to load navbar HTML
-  function loadNavbar() {
-    fetch("src/components/navbar/navbar.html")
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById("navbar").innerHTML = data;
-        addActiveClass(); // Add active class to current page link
-        setupToggleButton(); // Setup toggle button click event
-        setupScrollBehavior(); // Setup scroll behavior
-      })
-      .catch((error) => console.log("Error loading navbar:", error));
-  }
-
-  // Function to add active class to current page link
-  function addActiveClass() {
-    var currentLocation = window.location.href;
-    var navLinks = document.querySelectorAll("#navbar a");
-
-    navLinks.forEach(function (link) {
-      if (link.href === currentLocation) {
-        link.classList.add("active");
-      }
-    });
-  }
-
   function setupToggleButton() {
     const mobileMenu = document.querySelector("header nav .mobile-menu");
     const menu = document.querySelector("header nav .menu");
@@ -37,20 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
-
-  function setupScrollBehavior() {
-    const header = document.querySelector("header");
-    window.onscroll = function () {
-      if (document.body.scrollTop >= 200) {
-        header.classList.add("scrolled");
-      } else {
-        header.classList.remove("scrolled");
-      }
-    };
-  }
-
-  // Load the navbar when the DOM is ready
-  loadNavbar();
 });
 
 // Hero button smooth scroll to #recent
