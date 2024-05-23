cafb7570ef7c37040c09cfb43e69b95f4dc89789
Ozik Jarwo <jarwoozik08@gmail.com>
2024-05-23 14:21:18 +0700

    Contact us to WA

--- src/js/all.js
+++ src/js/all.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-document.querySelector('.mobile-nav').addEventListener('click', () => {
+  document.querySelector('.mobile-nav').addEventListener('click', () => {
     document.querySelector('header nav').classList.toggle('show');
   });
 });
